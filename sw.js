f6781bd815eeacac214188cdaf1e5b0c43c94ace
rbozzhanov-web <rbozzhanov@gmail.com>
2026-09-01 07:00:47 +0500

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="fuel-window-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./sw.js"]))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
