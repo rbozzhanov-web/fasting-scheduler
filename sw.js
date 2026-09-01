b6882e65574701880317947e3be34f0ba39e2c27
rbozzhanov-web <rbozzhanov@gmail.com>
2026-09-01 22:32:57 +0500

    Keep PWA cache version aligned with app version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const P="fuel-window-",C=P+"v31";
+const P="fuel-window-",C=P+"v30";
 const A=["./","./index.html","./roster-parser.js","./circadian.js","./timer.js","./state.js","./icon.svg","./icon-180.png","./icon-512.png","./manifest.webmanifest","./vendor/pdf.mjs","./vendor/pdf.worker.mjs","./vendor/PDFJS-LICENSE.txt","./sw.js"];
 
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
